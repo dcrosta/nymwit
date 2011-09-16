1caf4e0893354ee70598803a3c003e28f9f1a2d5
Dan Crosta <dcrosta@gmail.com>
2011-09-16 07:21:54 -0400

    remove NymwitUser, never used

--- nymwit/account/models.py
+++ nymwit/account/models.py
@@ -23,11 +23,3 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from mongoengine.django.auth import User
-from mongoengine import fields
-
-class NymwitUser(User):
-    high_score = fields.IntField()
-    total_score = fields.IntField()
-
-
